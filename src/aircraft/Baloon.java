2db59e86c7f97d3c414accaaca756ce513d5578d
naerhy <naerhy@protonmail.com>
2024-07-21 08:28:44 +0200

    Fix wrong newline's position in printf

--- src/aircraft/Baloon.java
+++ src/aircraft/Baloon.java
@@ -11,16 +11,16 @@ public class Baloon extends Aircraft {
   public void updateConditions() {
     String currentWeather = weatherTower.getWeather(coordinates);
     if (currentWeather == "fog") {
-      System.out.printf("%s#%s(%d): it's foggy!", "Baloon%n", name, id);
+      System.out.printf("%s#%s(%d): it's foggy!%n", "Baloon", name, id);
       coordinates.modifyHeightBy(-3);
     } else if (currentWeather == "rain") {
-      System.out.printf("%s#%s(%d): it's raining!", "Baloon%n", name, id);
+      System.out.printf("%s#%s(%d): it's raining!%n", "Baloon", name, id);
       coordinates.modifyHeightBy(-5);
     } else if (currentWeather == "snow") {
-      System.out.printf("%s#%s(%d): it's snowing!", "Baloon%n", name, id);
+      System.out.printf("%s#%s(%d): it's snowing!%n", "Baloon", name, id);
       coordinates.modifyHeightBy(-15);
     } else {
-      System.out.printf("%s#%s(%d): it's sunny!", "Baloon%n", name, id);
+      System.out.printf("%s#%s(%d): it's sunny!%n", "Baloon", name, id);
       coordinates.modifyLongitudeBy(2);
       coordinates.modifyHeightBy(4);
     }
--- src/aircraft/Helicopter.java
+++ src/aircraft/Helicopter.java
@@ -11,16 +11,16 @@ public class Helicopter extends Aircraft {
   public void updateConditions() {
     String currentWeather = weatherTower.getWeather(coordinates);
     if (currentWeather == "fog") {
-      System.out.printf("%s#%s(%d): it's foggy!", "Helicopter%n", name, id);
+      System.out.printf("%s#%s(%d): it's foggy!%n", "Helicopter", name, id);
       coordinates.modifyLongitudeBy(1);
     } else if (currentWeather == "rain") {
-      System.out.printf("%s#%s(%d): it's raining!", "Helicopter%n", name, id);
+      System.out.printf("%s#%s(%d): it's raining!%n", "Helicopter", name, id);
       coordinates.modifyLongitudeBy(5);
     } else if (currentWeather == "snow") {
-      System.out.printf("%s#%s(%d): it's snowing!", "Helicopter%n", name, id);
+      System.out.printf("%s#%s(%d): it's snowing!%n", "Helicopter", name, id);
       coordinates.modifyHeightBy(-12);
     } else {
-      System.out.printf("%s#%s(%d): it's sunny!", "Helicopter%n", name, id);
+      System.out.printf("%s#%s(%d): it's sunny!%n", "Helicopter", name, id);
       coordinates.modifyLongitudeBy(10);
       coordinates.modifyHeightBy(2);
     }
